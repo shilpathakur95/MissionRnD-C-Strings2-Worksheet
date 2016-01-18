c224786d2a558734bc6c764c01209fe1253f40d7
shilpathakur95 <shilpathakur95@missionrnd.com>
2016-01-18 21:07:20 +0530

    Submit Commit

--- src/getFrequencyofWord.cpp
+++ src/getFrequencyofWord.cpp
@@ -13,10 +13,61 @@ Note: Dont modify original str or word,Just return count ,Spaces can also be par
 #include <stdlib.h>
 
 int count_word_in_str_way_1(char *str, char *word){
-	return 0;
+	int iterator1=0, iterator2=0,iterator3;
+	int count = 0,len=0;
+	while (word[iterator2])
+	{
+		len++;
+		iterator2++;
+	}
+	iterator2 = 0;
+	while (str[iterator1])
+	{
+		iterator3 = iterator1 + 1;
+		iterator2 = 0;
+		while (word[iterator2])
+		{
+			if (str[iterator1] == word[iterator2])
+			{
+				iterator2++;
+				iterator1++;
+			}
+			else
+			{
+				iterator1++;
+				break;
+			}
+		}
+		if (iterator2 == len)
+		{
+			count++;
+		}
+		iterator1 = iterator3;
+	}
+	return count;
 }
 
 int count_word_int_str_way_2_recursion(char *str, char *word){
-	return 0;
+	if (str == NULL || word == NULL)
+		return 0;
+	int iterator = 0;
+	if (str[iterator] == '\0')
+		return 0;
+	while (word[iterator] && str[iterator])
+	{
+		if (str[iterator] == word[iterator])
+		{
+			iterator++;
+		}
+		else
+			break;
+	}
+	if (word[iterator] == '\0')
+	{
+		return 1 + count_word_int_str_way_2_recursion(str + 1, word);
+	}
+	else
+		return count_word_int_str_way_2_recursion(str + 1, word);
 }
+
 
